54d26ce79f13994b913198ddb27f71794d815634
梁晨阳 <lcyurban@163.com>
2024-01-26 15:33:31 +0800

    feat: init---sass、polyfill、elementPlus、postcss

---
+++ src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+
+onMounted(() => {
+    const arr = [1, 3, 4]
+    console.log(arr.at(-1))
+})
+</script>
+
+<template>
+    <div class="home">
+        <div class="home-box">测试</div>
+        <el-button type="primary" round>Primary</el-button>
+    </div>
+</template>
+<style scoped lang="scss">
+.home {
+    width: 200px;
+    height: 300px;
+    display: flex;
+    flex-flow: row nowrap;
+
+    &-box {
+        height: 100%;
+        width: 100%;
+    }
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,54 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+// css 浏览器补全
+import autoprefixer from 'autoprefixer'
+// 兼容旧版本浏览器
+import legacy from '@vitejs/plugin-legacy'
+
+// elementPlus按需加载相关配置
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        vue(),
+        vueJsx(),
+        legacy({
+            // targets: ['defaults', 'not IE 11'] 不设置此项时，将加载.browserslistrc
+        }),
+        // elementPlus按需加载 start
+        AutoImport({
+            resolvers: [ElementPlusResolver()]
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()]
+        })
+        // elementPlus按需加载 end
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    css: {
+        postcss: {
+            plugins: [
+                autoprefixer({
+                    grid: true
+                })
+            ]
+        },
+        // 配置 CSS 预处理器的选项
+        preprocessorOptions: {
+            scss: {
+                // additionalData 选项允许我们在每个 Sass 文件的顶部引入指定的 SCSS 文件
+                // additionalData: `@import "@/assets/var.scss";` // 这样，定义的变量就可以在任何地方使用了
+            }
+        }
+    }
+})
